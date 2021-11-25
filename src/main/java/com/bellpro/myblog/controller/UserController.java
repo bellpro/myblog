0f43855793fe3c51cdb86d80a8e1b4965742d077
bellpro <kju1909020@gmail.com>
2021-11-25 22:23:37 +0900

    로그인 부분 완성

--- src/main/java/com/bellpro/myblog/controller/UserController.java
+++ src/main/java/com/bellpro/myblog/controller/UserController.java
@@ -2,11 +2,14 @@ package com.bellpro.myblog.controller;
 
 import com.bellpro.myblog.domain.dto.SignupRequestDto;
 import com.bellpro.myblog.service.UserService;
+import com.bellpro.myblog.validator.SignupRequestDtoValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -16,6 +19,12 @@ import javax.validation.Valid;
 @Controller // 자동 응답기, controller 명시, @Component 포항: Bean 등록
 public class UserController {
     private final UserService userService;  // controller -> service 로 dto 전달하기 위해 사용
+    private final SignupRequestDtoValidator signupRequestDtoValidator;  // 닉네임 중복 검사, 닉네임과 비밀번호 일치 확인
+
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder){
+        webDataBinder.setValidator(this.signupRequestDtoValidator); // 검증 결과를 BindingResult 넣음
+    }
 
     @GetMapping("/signup")
     public String signup(Model model) {
@@ -30,6 +39,12 @@ public class UserController {
         }
         userService.registerUser(signupRequestDto); // service 로 dto 전달
 
-        return "redirect:/user/login";   // 로그인 페이지 (templates/user/login.html) 이동
+        return "redirect:/login";   // 로그인 페이지 (templates/user/login.html) 이동
     }
+
+    @GetMapping("/login")
+    public String login() {
+        return "user/login"; // 로그인 페이지 이동
+    }
+
 }
